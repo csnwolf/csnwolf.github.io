7505aa38c24c9cfd5e2cb159b04943a67df85efa
csnwolf <christianwolf021@gmail.com>
2021-03-25 17:58:18 +0100

    forschleife befüllen

--- nz/main.js
+++ nz/main.js
@@ -16,9 +16,16 @@ const map = L.map("map", {
     ]
 });
 
+let nav = document.querySelector("#navigation");
+//console.log(nav);
+
 //console.log(ROUTE);
 for (let entry of ROUTE) {
     //console.log(entry);
+
+    nav.innerHTML += `
+        <option value="${entry.user}">Stop ${entry.nr}: ${entry.name}</option>
+    `;
 
     let mrk = L.marker([entry.lat, entry.lng ]).addTo(map);
     mrk.bindPopup(`
@@ -32,6 +39,6 @@ for (let entry of ROUTE) {
     }
 }
 
-//<option value="csnwolf">Wanaka</option>
+//
 
 //console.log(document.querySelector("#map"));
