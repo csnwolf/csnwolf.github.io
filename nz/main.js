f5553e2ed43b81db682c10e62ca3c0f7532c30dd
csnwolf <christianwolf021@gmail.com>
2021-03-25 16:17:23 +0100

    Karte erstellt mit center, zoom und layer

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,11 @@
-const map = L.map("map");
+const map = L.map("map", {
+    center: [-44.7, 169.15 ],
+    zoom: 13,
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+});
+
+// WMTS-Services
+
 console.log(document.querySelector("#map"));
