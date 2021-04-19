242fadb23685c8712a532a9ca3dcb9bc3d60abc9
csnwolf <christianwolf021@gmail.com>
2021-04-19 12:59:23 +0200

    Sort verbessert

--- nz/main.js
+++ nz/main.js
@@ -20,7 +20,11 @@ let nav = document.querySelector("#navigation");
 //console.log(nav);
 
 ROUTE.sort((stop1, stop2) => {
-    return stop1.nr > stop2.nr
+    if (stop1.nr > stop2.nr) {
+        return 1;
+    } else {
+        return -1;
+    }
 });
 
 for (let entry of ROUTE) {
